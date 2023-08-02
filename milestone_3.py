a5716c06f61cf62f0f1dafe31e873570dea95c16
LDC-creator <131806469+LDC-creator@users.noreply.github.com>
2023-08-02 16:22:56 +0100

    put the code into functions

--- milestone_3.py
+++ milestone_3.py
@@ -1,1 +1,22 @@
-fff
+import random
+
+word_list = ["grapes", "oranges", "apples", "blueberries", "strawberries"]
+word = random.choice(word_list)
+
+def check_guess(guess):
+    guess = guess.lower()
+
+    if guess in word:
+        print(f"Good guess! {guess} is in the word. {word}")
+    else:
+        print(f"Sorry, {guess} is not in the word. Try again. {word}")
+
+def ask_for_input():
+    while True:
+        guess = input("Guess a letter please. ")
+        if guess.isalpha() and len(guess) == 1:
+            check_guess(guess)
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+
+ask_for_input()
